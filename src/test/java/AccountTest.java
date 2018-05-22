308e1fc1b797f44f8932d5346f706d1c5ae41bb8
Mateusz Drozdzik <drozdz91@interia.pl>
2018-05-22 17:28:04 +0200

    Updated AccountTest class

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,14 +1,22 @@
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.AccountPage;
 import scenarios.LoginScenario;
 import scenarios.OpenNewAccountScenario;
 
 
 public class AccountTest extends MainTest {
 
+    private AccountPage accountPage;
+
+    @BeforeMethod
+    public void beforeTest() {
+        accountPage = indexPage.run(new LoginScenario("Mat", "test"));
+    }
+
     @Test
     public void shouldAddAccount() {
-        indexPage.run(new LoginScenario("Mat", "test"))
-                .menu.run(new OpenNewAccountScenario("CHECKING", "14010"))
+                accountPage.menu.run(new OpenNewAccountScenario("CHECKING", "14010"))
                 .openAccountAssertion.getAccountOpenedConfirmation();
     }
 }
